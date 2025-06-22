c50e3d009a357a133784731d411202d2802a028f
web-mech <webmech@gmail.com>
2025-06-21 23:07:28 -0400

    feat(images): allow serving images from external domain
    - Added configuration to allow serving images from external domains by adding the specified domain to the list of allowed domains in the `next.config.ts` file.

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
     // This will completely ignore TypeScript errors during builds
     ignoreBuildErrors: true,
   },
+  images: {
+    // Allow images from the public directory
+    unoptimized: false,
+    // If you need to serve images from external domains, add them here
+    domains: [
+      "pub-7f0ccf30c0134c51b5d048e704c7b9b0.r2.dev"
+    ],
+  },
 };
 
 export default nextConfig;
